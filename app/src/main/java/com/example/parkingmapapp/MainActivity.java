95e63fad46e80dba140ce2b87e565d92e243af1a
DanBed666 <daniel@blockether.io>
2024-05-02 21:13:42 +0200

    Refactor code to make login and register activities

--- app/src/main/java/com/example/parkingmapapp/MainActivity.java
+++ app/src/main/java/com/example/parkingmapapp/MainActivity.java
@@ -1,242 +1,31 @@
 package com.example.parkingmapapp;
 
-import android.app.FragmentManager;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
-import org.osmdroid.api.IMapController;
-import org.osmdroid.bonuspack.kml.KmlDocument;
-import org.osmdroid.bonuspack.location.OverpassAPIProvider;
-import org.osmdroid.config.Configuration;
-import org.osmdroid.events.MapEventsReceiver;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.BoundingBox;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.FolderOverlay;
-import org.osmdroid.views.overlay.MapEventsOverlay;
-import org.osmdroid.views.overlay.compass.CompassOverlay;
-import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
-
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity implements MapEventsReceiver
+public class MainActivity extends AppCompatActivity
 {
-    private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
-    private MapView map = null;
-    Button location;
-    Button find;
-    MyLocationNewOverlay mLocationOverlay;
-    InfoFragment fragment;
-    FragmentTransaction ft;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+        setContentView(R.layout.activity_main);
 
         setContentView(R.layout.activity_main);
 
-        //handle permissions first, before map is created. not depicted here
-
-        //load/initialize the osmdroid configuration, this can be done
-        Context ctx = getApplicationContext();
-        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
-        //setting this before the layout is inflated is a good idea
-        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
-        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
-        //see also StorageUtils
-        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's
-        //tile servers will get you banned based on this string
-
-        //inflate and create the map
-        setContentView(R.layout.activity_main);
-
-        map = (MapView) findViewById(R.id.map);
-        map.setTileSource(TileSourceFactory.MAPNIK);
-
-        mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(getApplicationContext()),map);
-
-        String [] permissions = new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.WRITE_EXTERNAL_STORAGE};
-
-        requestPermissionsIfNecessary(permissions
-                // if you need to show the current location, uncomment the line below
-                // Manifest.permission.ACCESS_FINE_LOCATION,
-                // WRITE_EXTERNAL_STORAGE is required in order to show the map
-        );
-
-        map.setBuiltInZoomControls(true);
-        map.setMultiTouchControls(true);
-
-        showMyLocation();
-        showCompass();
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) ->
+        {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        location = findViewById(R.id.btn_location);
-        find = findViewById(R.id.btn_find);
-
-        location.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                navToLocation(mLocationOverlay.getMyLocation());
-            }
-        });
-
-        find.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                findParkings(mLocationOverlay.getMyLocation());
-            }
-        });
-
-        fragment = new InfoFragment();
-
-        MapEventsOverlay mapEventsOverlay = new MapEventsOverlay(this);
-        map.getOverlays().add(mapEventsOverlay);
-    }
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
-    }
-
-    @Override
-    public void onPause()
-    {
-        super.onPause();
-        //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().save(this, prefs);
-        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
-    {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        ArrayList<String> permissionsToRequest = new ArrayList<>();
-        for (int i = 0; i < grantResults.length; i++)
-        {
-            permissionsToRequest.add(permissions[i]);
-        }
-        if (!permissionsToRequest.isEmpty())
-        {
-            ActivityCompat.requestPermissions(
-                    this,
-                    permissionsToRequest.toArray(new String[0]),
-                    REQUEST_PERMISSIONS_REQUEST_CODE);
-        }
-    }
-
-    private void requestPermissionsIfNecessary(String[] permissions)
-    {
-        ArrayList<String> permissionsToRequest = new ArrayList<>();
-        for (String permission : permissions)
-        {
-            if (ContextCompat.checkSelfPermission(this, permission)
-                    != PackageManager.PERMISSION_GRANTED)
-            {
-                // Permission is not granted
-                permissionsToRequest.add(permission);
-            }
-        }
-        if (!permissionsToRequest.isEmpty())
-        {
-            ActivityCompat.requestPermissions(
-                    this,
-                    permissionsToRequest.toArray(new String[0]),
-                    REQUEST_PERMISSIONS_REQUEST_CODE);
-        }
-    }
-
-    public void showMyLocation()
-    {
-        mLocationOverlay.enableFollowLocation();
-        map.getOverlays().add(mLocationOverlay);
-        map.getController().setZoom(18.0);
-    }
-
-    public void showCompass()
-    {
-        CompassOverlay mCompassOverlay = new CompassOverlay(getApplicationContext(), new InternalCompassOrientationProvider(getApplicationContext()), map);
-        mCompassOverlay.enableCompass();
-        map.getOverlays().add(mCompassOverlay);
-    }
-
-    public void navToLocation(GeoPoint location)
-    {
-        map.getController().setCenter(location);
-    }
-
-    public void findParkings(GeoPoint location)
-    {
-        OverpassAPIProvider overpassProvider = new OverpassAPIProvider();
-        BoundingBox range = new BoundingBox(location.getLatitude() + 0.05, location.getLongitude() + 0.05,
-                location.getLatitude() - 0.05, location.getLongitude() - 0.05);
-        String url = overpassProvider.urlForTagSearchKml("amenity=parking", range, 500, 30);
-        KmlDocument kmlDocument = new KmlDocument();
-        boolean ok = overpassProvider.addInKmlFolder(kmlDocument.mKmlRoot, url);
-        FolderOverlay kmlOverlay = (FolderOverlay) kmlDocument.mKmlRoot.buildOverlay(map, null, null, kmlDocument);
-        map.getOverlays().add(kmlOverlay);
-        map.getOverlays().add(kmlOverlay);
-    }
-
-    @Override
-    public boolean singleTapConfirmedHelper(GeoPoint p)
-    {
-        Log.i("SINGLE", "Single tap");
-        getSupportFragmentManager().beginTransaction().remove(fragment).commit();
-
-        return true;
-    }
-
-    @Override
-    public boolean longPressHelper(GeoPoint p)
-    {
-        Log.i("LONG", "Long tap");
-
-        ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment, fragment);
-        ft.commit();
-
-        return true;
     }
 }
