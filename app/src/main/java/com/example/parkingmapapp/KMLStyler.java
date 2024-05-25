bb50788e36e24eca9eaf0534c3f05c8b681b95ca
DanBed666 <daniel@blockether.io>
2024-05-25 21:20:07 +0200

    Refactor code in KMLStyler

--- app/src/main/java/com/example/parkingmapapp/KMLStyler.java
+++ app/src/main/java/com/example/parkingmapapp/KMLStyler.java
@@ -35,7 +35,6 @@ import java.util.Objects;
 public class KMLStyler implements KmlFeature.Styler
 {
     InfoFragment fragment;
-    FragmentTransaction ft;
     Utils u;
     Context context;
     MapView map;
@@ -62,88 +61,14 @@ public class KMLStyler implements KmlFeature.Styler
     @Override
     public void onPoint(Marker marker, KmlPlacemark kmlPlacemark, KmlPoint kmlPoint)
     {
-        String id = kmlPlacemark.mId;
-        String nm = "Brak";
-        String pk = kmlPlacemark.getExtendedData("parking");
-        String cpc = kmlPlacemark.getExtendedData("capacity");;
-        String fee = kmlPlacemark.getExtendedData("fee");;
-        String svd = kmlPlacemark.getExtendedData("supervised");;
-        String ope = kmlPlacemark.getExtendedData("operator");;
-
-        DatabaseReference p = parkings.child(id);
-
-        Log.i("KLUCZ", id + "   " + Objects.requireNonNull(p.getKey()));
-
-        parkings.addListenerForSingleValueEvent(new ValueEventListener()
-        {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot)
-            {
-                Log.i("KLUCZ2", String.valueOf(snapshot.hasChild(id)));
-
-                if (!snapshot.hasChild(id))
-                {
-                    Log.i("DODANO", "dodano");
-                    parking = new Parking(nm, pk, cpc, fee, svd, ope);
-                    parkings.child(id).setValue(parking);
-                }
-                else
-                {
-                    Log.i("NIEDODANO", "niedodano");
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error)
-            {
-
-            }
-        });
-
-        String id2 = kmlPlacemark.getExtendedData("parking");
-        Log.i("LATITUDE", String.valueOf(kmlPoint.getPosition().getLatitude()));
-        Log.i("LONGITUDE", String.valueOf(kmlPoint.getPosition().getLongitude()));
+        String id = addParkings(kmlPlacemark);
 
         marker.setOnMarkerClickListener(new Marker.OnMarkerClickListener()
         {
             @Override
             public boolean onMarkerClick(Marker marker, MapView mapView)
             {
-                fragment = new InfoFragment();
-                u = new Utils(context, map, startPoint, marker.getPosition());
-
-                Bundle bundle = new Bundle();
-                bundle.putSerializable("OBJECT", u);
-                bundle.putSerializable("PARKING", parking);
-                bundle.putString("KEYID", id);
-                fragment.setArguments(bundle);
-
-                listener.getSupportFM().beginTransaction().
-                        setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
-                        .show(fragment)
-                        .replace(R.id.fragment, fragment)
-                        .commit();
-
-                MapEventsOverlay mapEventsOverlay = new MapEventsOverlay(new MapEventsReceiver() {
-                    @Override
-                    public boolean singleTapConfirmedHelper(GeoPoint p)
-                    {
-                        u.clearRoute();
-                        listener.getSupportFM().beginTransaction().
-                                setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
-                                .hide(fragment)
-                                .commit();
-                        return false;
-                    }
-
-                    @Override
-                    public boolean longPressHelper(GeoPoint p)
-                    {
-                        return false;
-                    }
-                });
-
-                map.getOverlays().add(mapEventsOverlay);
+                addFragment(marker.getPosition(), id);
 
                 return true;
             }
@@ -158,13 +83,35 @@ public class KMLStyler implements KmlFeature.Styler
     @Override
     public void onPolygon(Polygon polygon, KmlPlacemark kmlPlacemark, KmlPolygon kmlPolygon)
     {
+        String id = addParkings(kmlPlacemark);
+
+        polygon.setOnClickListener(new Polygon.OnClickListener()
+        {
+            @Override
+            public boolean onClick(Polygon polygon, MapView mapView, GeoPoint eventPos)
+            {
+                addFragment(eventPos, id);
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onTrack(Polyline polyline, KmlPlacemark kmlPlacemark, KmlTrack kmlTrack)
+    {
+
+    }
+
+    public String addParkings(KmlPlacemark kmlPlacemark)
+    {
         String id = kmlPlacemark.mId;
         String nm = "Brak";
         String pk = kmlPlacemark.getExtendedData("parking");
-        String cpc = kmlPlacemark.getExtendedData("capacity");;
-        String fee = kmlPlacemark.getExtendedData("fee");;
-        String svd = kmlPlacemark.getExtendedData("supervised");;
-        String ope = kmlPlacemark.getExtendedData("operator");;
+        String cpc = kmlPlacemark.getExtendedData("capacity");
+        String fee = kmlPlacemark.getExtendedData("fee");
+        String svd = kmlPlacemark.getExtendedData("supervised");
+        String ope = kmlPlacemark.getExtendedData("operator");
 
         DatabaseReference p = parkings.child(id);
 
@@ -196,55 +143,46 @@ public class KMLStyler implements KmlFeature.Styler
             }
         });
 
-        polygon.setOnClickListener(new Polygon.OnClickListener()
-        {
-            @Override
-            public boolean onClick(Polygon polygon, MapView mapView, GeoPoint eventPos)
-            {
-                fragment = new InfoFragment();
-                u = new Utils(context, map, startPoint, eventPos);
-
-                Bundle bundle = new Bundle();
-                bundle.putSerializable("OBJECT", u);
-                bundle.putSerializable("PARKING", parking);
-                bundle.putString("KEYID", id);
-                fragment.setArguments(bundle);
-
-                listener.getSupportFM().beginTransaction().
-                        setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
-                        .show(fragment)
-                        .replace(R.id.fragment, fragment)
-                        .commit();
-
-                MapEventsOverlay mapEventsOverlay = new MapEventsOverlay(new MapEventsReceiver() {
-                    @Override
-                    public boolean singleTapConfirmedHelper(GeoPoint p)
-                    {
-                        u.clearRoute();
-                        listener.getSupportFM().beginTransaction().
-                                setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
-                                .hide(fragment)
-                                .commit();
-                        return false;
-                    }
-
-                    @Override
-                    public boolean longPressHelper(GeoPoint p)
-                    {
-                        return false;
-                    }
-                });
-
-                map.getOverlays().add(mapEventsOverlay);
-
-                return true;
-            }
-        });
+        return id;
     }
 
-    @Override
-    public void onTrack(Polyline polyline, KmlPlacemark kmlPlacemark, KmlTrack kmlTrack)
+    public void addFragment(GeoPoint geoPoint, String id)
     {
+        fragment = new InfoFragment();
+        u = new Utils(context, map, startPoint, geoPoint);
 
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("OBJECT", u);
+        bundle.putSerializable("PARKING", parking);
+        bundle.putString("KEYID", id);
+        fragment.setArguments(bundle);
+        Log.i("IDFRAG", id);
+
+        listener.getSupportFM().beginTransaction().
+                setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+                .show(fragment)
+                .replace(R.id.fragment, fragment)
+                .commit();
+
+        MapEventsOverlay mapEventsOverlay = new MapEventsOverlay(new MapEventsReceiver() {
+            @Override
+            public boolean singleTapConfirmedHelper(GeoPoint p)
+            {
+                u.clearRoute();
+                listener.getSupportFM().beginTransaction().
+                        setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+                        .hide(fragment)
+                        .commit();
+                return false;
+            }
+
+            @Override
+            public boolean longPressHelper(GeoPoint p)
+            {
+                return false;
+            }
+        });
+
+        map.getOverlays().add(mapEventsOverlay);
     }
 }
