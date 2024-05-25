8ae622e2cd07c59a3d4f16dc8793659a4141aeb4
DanBed666 <daniel@blockether.io>
2024-05-25 14:22:03 +0200

    Add change email and password options

--- app/src/main/java/com/example/parkingmapapp/EmailChangeActivity.java
+++ app/src/main/java/com/example/parkingmapapp/EmailChangeActivity.java
@@ -1,20 +1,35 @@
 package com.example.parkingmapapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
 public class EmailChangeActivity extends AppCompatActivity
 {
+    EditText email;
+    EditText password;
     EditText newEmail;
     Button changeEmail;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -27,14 +42,54 @@ public class EmailChangeActivity extends AppCompatActivity
             return insets;
         });
 
-        newEmail = findViewById(R.id.et_email);
+        email = findViewById(R.id.et_email);
+        password = findViewById(R.id.old_pass);
+        newEmail = findViewById(R.id.et_newemail);
         changeEmail = findViewById(R.id.btn_change);
+        FirebaseUser user = mAuth.getCurrentUser();
 
         changeEmail.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick(View v) {
-
+            public void onClick(View v)
+            {
+                assert user != null;
+                String emailAdr = email.getText().toString();
+                String passwordOld = password.getText().toString();
+                AuthCredential credential = EmailAuthProvider.getCredential(emailAdr, passwordOld);
+                user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            assert user != null;
+                            String email = newEmail.getText().toString();
+                            user.verifyBeforeUpdateEmail(email).addOnCompleteListener(new OnCompleteListener<Void>()
+                            {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task)
+                                {
+                                    if (task.isSuccessful())
+                                    {
+                                        Toast.makeText(getApplicationContext(), "Zmieniono adres email pomyślnie", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(getApplicationContext(), "Błąd w trakcie zmiany maila", Toast.LENGTH_SHORT).show();
+                                        Log.e("ERROR", Objects.requireNonNull(Objects.requireNonNull(task.getException()).getMessage()));
+                                    }
+                                }
+                            });
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), "Błąd w trakcie zmiany maila", Toast.LENGTH_SHORT).show();
+                            Log.e("ERROR", Objects.requireNonNull(Objects.requireNonNull(task.getException()).getMessage()));
+                        }
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/example/parkingmapapp/PasswordChangeActivity.java
+++ app/src/main/java/com/example/parkingmapapp/PasswordChangeActivity.java
@@ -1,21 +1,35 @@
 package com.example.parkingmapapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
 public class PasswordChangeActivity extends AppCompatActivity
 {
+    EditText email;
     EditText oldPassword;
     EditText newPassword;
     Button changePassword;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -28,16 +42,53 @@ public class PasswordChangeActivity extends AppCompatActivity
             return insets;
         });
 
+        email = findViewById(R.id.et_email);
         oldPassword = findViewById(R.id.old_pass);
         newPassword = findViewById(R.id.new_pass);
         changePassword = findViewById(R.id.btn_change);
+        FirebaseUser user = mAuth.getCurrentUser();
 
         changePassword.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
+                assert user != null;
+                String emailAdr = email.getText().toString();
+                String passwordOld = oldPassword.getText().toString();
+                AuthCredential credential = EmailAuthProvider.getCredential(emailAdr, passwordOld);
+                user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>()
+                {
+                    final String passwordNew = newPassword.getText().toString();
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            user.updatePassword(passwordNew).addOnCompleteListener(new OnCompleteListener<Void>()
+                            {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task)
+                                {
+                                    if (task.isSuccessful())
+                                    {
+                                        Toast.makeText(getApplicationContext(), "Zmieniono hasło pomyślnie", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(getApplicationContext(), "Błąd w trakcie zmiany hasła", Toast.LENGTH_SHORT).show();
+                                        Log.e("ERROR", Objects.requireNonNull(Objects.requireNonNull(task.getException()).getMessage()));
+                                    }
+                                }
+                            });
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), "Błąd w trakcie zmiany hasła", Toast.LENGTH_SHORT).show();
+                            Log.e("ERROR", Objects.requireNonNull(Objects.requireNonNull(task.getException()).getMessage()));
+                        }
+                    }
+                });
             }
         });
     }
